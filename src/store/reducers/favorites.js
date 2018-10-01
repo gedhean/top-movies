e7cd9f198bea593716d688039d949553ecdba691
gedhean <gedhean.dev@gmail.com>
2018-09-30 22:39:10 -0300

    Colocando o redux...

---
+++ src/store/reducers/favorites.js
@@ -0,0 +1,12 @@
+const favorites = (state, action) => {
+    switch (action.type) {
+        case 'value':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+export default favorites;
