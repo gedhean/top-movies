77a75b77fdbe3576d8da1dec9a253d1ea7d6473c
Gêdhean Alves Vieira <gedhean.alves@secrel.net.br>
2018-10-02 07:50:54 -0300

    Overrides configs atualizadas

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+// Override some wabpack configs
+const path = require('path')
+const { rewireWorkboxInject, defaultInjectConfig } = require('react-app-rewire-workbox')
+
+module.exports = function override(config, env) {
+  if (env === 'production') {
+    console.log('Production build - Adding Workbox for PWAs')
+    // Extend the default injection config with required swSrc
+    const workboxConfig = {
+      ...defaultInjectConfig,
+      swSrc: path.join(__dirname, 'src', 'sw.js'),
+      importWorkboxFrom: 'local'
+    }
+    config = rewireWorkboxInject(workboxConfig)(config, env)
+  }
+
+  return config
+}
